4443f4e2d139c3c8f81d0695aa408610d38a170c
Pedro Nascimento <pedro.nascimento@bbce.com.br>
2021-02-13 17:03:26 -0300

    Adiciona projeto com cypress

---
+++ cypress/integration/api/users.spec.js
@@ -0,0 +1,111 @@
+/// <reference types="cypress" />
+
+import constants from '../../support/constants'
+
+describe('Manipulando Usuário', () => {
+  let token = `Bearer 2275e2cbbf8dc1d113b25fb018cdb2e07e088b35bb5f7b7c13ca160ed96a82ba`;
+  let id;
+
+  describe('POST de criar Usuario, GET, para listar o usuario, PUT alterar usuario, e DELETE deletar usuário', () => {
+    before(('Post criar usuário', () => {
+      cy.request({
+        method: 'POST',
+        url: '/',
+        body: {
+          name: constants.RANDOM_NAME,
+          email: constants.RANDOM_EMAIL,
+          gender: 'Male',
+          status: 'Active'
+        },
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.data.name).to.eq(constants.RANDOM_NAME)
+        id = response.body.data.id
+      })
+    }))
+
+    it('GET - Retornar usuário criado no POST de criar usuário', () => {
+      cy.request({
+        method: 'GET',
+        url: '/',
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        qs: { 'name': constants.RANDOM_NAME }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.data[0].name).to.eq(constants.RANDOM_NAME)
+      })
+    })
+  })
+
+  describe('PUT - Editar e visualizar usuário editado', () => {
+    it('PUT - Alterar usuário criado no before', () => {
+      cy.request({
+        method: 'PUT',
+        url: '/' + id,
+        body: {
+          name: 'Pedro alterei aqui',
+        },
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.data.name).to.eq('Pedro alterei aqui')
+      })
+    })
+
+    it('GET - Retornar usuário alterado no PUT', () => {
+      cy.request({
+        method: 'GET',
+        url: '/' + id,
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.data.name).to.eq('Pedro alterei aqui')
+        expect(response.body.data.id).to.eq(id)
+      })
+    })
+  })
+
+  describe('Excluindo e visualizando usuário editado', () => {
+    it('Excluir usuário criado', () => {
+      cy.request({
+        method: 'DELETE',
+        url: '/' + id,
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+
+    it('Retornar usuário excluído', () => {
+      cy.request({
+        method: 'GET',
+        url: '/' + id,
+        headers: {
+          Authorization: token,
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        qs: { 'name': 'Pedro alterei aqui' }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.data[0]).to.be.undefined
+      })
+    })
+  })
+
+})
